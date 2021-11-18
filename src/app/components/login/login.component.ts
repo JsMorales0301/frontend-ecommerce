c334260ac8f67eb38358a5ffa3233fafba15b9dd
Francisco <fjmoreno2101@gmail.com>
2021-11-18 16:35:42 -0500

    modal display added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+export class LoginComponent{
+
+  constructor(private primengConfig: PrimeNGConfig) { }
+
+  ngOnInit(): any {
+    this.primengConfig.ripple = true;
   }
 
+
+   displayModal: boolean = true;
+
+  showModalDialog() {
+    this.displayModal = true;
+}
 }
--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
-
 import {MenubarModule} from 'primeng/menubar';
-/* import { ButtonModule } from "primeng/button";
-import { PanelModule } from "primeng/panel";
-import { RippleModule } from "primeng/ripple"; */
 import { InputTextModule } from "primeng/inputtext";
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [],
   imports: [
-
+    BrowserModule,
+    BrowserAnimationsModule,
+    DialogModule,
+    ButtonModule
   ],
   exports: [
     MenubarModule,
-    InputTextModule
-  ]
+    InputTextModule,
+    DialogModule,
+    ButtonModule
+  ],
+  
 })
 export class PrimeNgModule { }
