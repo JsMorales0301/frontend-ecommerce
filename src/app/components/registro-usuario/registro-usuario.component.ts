aa816f079b92000bda9fe56cca5ccfbe35e4a293
Francisco <fjmoreno2101@gmail.com>
2021-11-30 17:08:50 -0500

    first commit Detailed Product Window

--- src/app/components/registro-usuario/registro-usuario.component.ts
+++ src/app/components/registro-usuario/registro-usuario.component.ts
@@ -11,37 +11,18 @@ import { NzFormTooltipIcon } from 'ng-zorro-antd/form';
 export class RegistroUsuarioComponent implements OnInit {
 
 
-  constructor(private fb: FormBuilder) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.validateForm = this.fb.group({
-      email: [null, [Validators.email, Validators.required]],
-      password: [null, [Validators.required]],
-      checkPassword: [null, [Validators.required, this.confirmationValidator]],
-      nickname: [null, [Validators.required]],
-      idType: ['Cc'],
-      identificationValid: [null, [Validators.required]],
-      website: [null, [Validators.required]],
-      captcha: [null, [Validators.required]],
-      agree: [false]
-    });
+    
+
    
   }
 
-  isOkLoading = false;
 
 
 
 // modal functionality
-
-
-  handleOk(): void {
-    this.isOkLoading = true;
-    setTimeout(() => {
-
-      this.isOkLoading = false;
-    }, 3000);
-  }
 
 
 // end modal functionality
@@ -49,46 +30,9 @@ export class RegistroUsuarioComponent implements OnInit {
 // form functionalities
 
 // password
-passwordVisible = false;
-password?: string;
 
 // typeid
-validateForm!: FormGroup;
-  captchaTooltipIcon: NzFormTooltipIcon = {
-    type: 'info-circle',
-    theme: 'twotone'
-  };
 
-  submitForm(): void {
-    if (this.validateForm.valid) {
-      console.log('submit', this.validateForm.value);
-    } else {
-      Object.values(this.validateForm.controls).forEach(control => {
-        if (control.invalid) {
-          control.markAsDirty();
-          control.updateValueAndValidity({ onlySelf: true });
-        }
-      });
-    }
-  }
-
-  updateConfirmValidator(): void {
-    /** wait for refresh value */
-    Promise.resolve().then(() => this.validateForm.controls.checkPassword.updateValueAndValidity());
-  }
-
-  confirmationValidator = (control: FormControl): { [s: string]: boolean } => {
-    if (!control.value) {
-      return { required: true };
-    } else if (control.value !== this.validateForm.controls.password.value) {
-      return { confirm: true, error: true };
-    }
-    return {};
-  };
-
-  getCaptcha(e: MouseEvent): void {
-    e.preventDefault();
-  }
 
 // end form functionalities
   
