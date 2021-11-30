ec300989c8dceb32feebfced645e687b602ab825
Francisco <fjmoreno2101@gmail.com>
2021-11-30 12:17:54 -0500

    login-as-form-feature

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -17,27 +17,21 @@ export class LoginComponent{
    
   }
 
-  isVisible = false;
+
   isOkLoading = false;
 
 
 
-// modal functionality
-  showModal(): void {
-    this.isVisible = true;
-  }
 
   handleOk(): void {
     this.isOkLoading = true;
     setTimeout(() => {
-      this.isVisible = false;
+    
       this.isOkLoading = false;
     }, 3000);
   }
 
-  handleCancel(): void {
-    this.isVisible = false;
-  }
+
 // end modal functionality
 
 // form functionality
