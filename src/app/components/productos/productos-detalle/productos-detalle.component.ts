d7f737c2e512c4d1ce8de6872a095f70a09a4652
Francisco <fjmoreno2101@gmail.com>
2021-12-01 18:33:53 -0500

    Updating product detailed window

--- src/app/components/productos/productos-detalle/productos-detalle.component.ts
+++ src/app/components/productos/productos-detalle/productos-detalle.component.ts
@@ -1,3 +1,4 @@
+import { prepareEventListenerParameters } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit } from '@angular/core';
 
 
@@ -11,22 +12,42 @@ export class ProductosDetalleComponent implements OnInit {
 
   panels = [
     {
-      active: true,
-      name: 'This is panel header 1',
-      disabled: false
+      active: false,
+      name: 'CARACTERÍSTICAS PRINCIPALES DEL PRODUCTO',
+      disabled: false,
+      mainFeaturesP: [{
+      detail1: 'Procesador AMD Ryzen Zen 2 con 8 núcleos, 16 subprocesos.',
+      detail2: 'Frecuencia variable de procesador hasta 3.5 GHz.',
+      detail3: 'Frecuencia variable de GPU hasta 2.23 GHz (10.3 TFLOPS).',
+      detail4: 'Memoria GDDR6 16 GB con ancho de banda de 448 GB/s.'
+      }]
     },
     {
       active: false,
       disabled: false,
-      name: 'This is panel header 2'
+      name: 'DETALLES DEL PRODUCTO',
+      mainDetailsProduct: [{
+        detail1: 'CPU: 8 núcleos Zen 2 a 3.5GHz (frecuencia variable)',
+        detail2: 'GPU 10.28 TFLOPs, 36 CUs a 2.23GHz (frecuencia variable)',
+        detail3: 'Arquitectura de la GPU RDNA 2 a medida',
+        detail4: 'Memoria/Interfaz 16GB GDDR6/256-bit',
+        detail5: 'Ancho de banda de la memoria 448GB/s',
+        detail6: 'Almacenamiento 825GB SSD a medida',
+        detail7: 'IO 5.5GB/s (Puro), 8-9GB/s de media (Comprimido)',
+        detail8: 'Ampliación de almacenamiento Slot NVMe SSD',
+        detail9: 'Almacenamiento externo Compatibilidad USB HDD',
+        detail10: 'Lector óptico 4K UHD Blu-ray'
+      }]
     },
     {
       active: false,
-      disabled: true,
-      name: 'This is panel header 3'
+      disabled: false,
+      name: 'RESEÑAS'
+
     }
   ];
 
+  
   constructor() { }
 
   ngOnInit(): void {
