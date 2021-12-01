a415166cbf8e1c81638feac3317722e6807cca01
Francisco <fjmoreno2101@gmail.com>
2021-12-01 13:30:36 -0500

    changing to normal version product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
 
 registerLocaleData(es);
 
@@ -72,7 +73,8 @@ const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesign
     ReactiveFormsModule,
     HttpClientJsonpModule,
     NzIconModule,
-    NzSelectModule
+    NzSelectModule,
+    NzCollapseModule
 
 
 
--- src/app/components/productos/productos-detalle/productos-detalle.component.ts
+++ src/app/components/productos/productos-detalle/productos-detalle.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-productos-detalle',
   templateUrl: './productos-detalle.component.html',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductosDetalleComponent implements OnInit {
 
+  panels = [
+    {
+      active: true,
+      name: 'This is panel header 1',
+      disabled: false
+    },
+    {
+      active: false,
+      disabled: false,
+      name: 'This is panel header 2'
+    },
+    {
+      active: false,
+      disabled: true,
+      name: 'This is panel header 3'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+
+   
   }
 
 }
